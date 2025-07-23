e6037049014a027f8c68b9b7f1475c72d365a9db
zollak <44733278+zollak@users.noreply.github.com>
2025-07-23 16:40:04 +0200

    add encoding support

--- custom_components/syslog_receiver/config_flow.py
+++ custom_components/syslog_receiver/config_flow.py
@@ -14,6 +14,8 @@ from .const import (
     DEFAULT_CERTFILE,
     DEFAULT_KEYFILE,
     MIN_SEVERITY_LEVELS,
+    DEFAULT_ENCODING,
+    COMMON_ENCODINGS,
 )
 
 STEP_USER_DATA_SCHEMA = vol.Schema(
@@ -28,21 +30,47 @@ STEP_USER_DATA_SCHEMA = vol.Schema(
         vol.Required("allowed_ips", default=DEFAULT_ALLOWED_IPS): str,
         vol.Required("min_severity", default=DEFAULT_MIN_SEVERITY): vol.In(tuple(MIN_SEVERITY_LEVELS.keys())),
         vol.Required("enable_sensors", default=False): bool,
+        vol.Optional("encoding", default=DEFAULT_ENCODING): vol.In(COMMON_ENCODINGS),
     }
 )
 
+CUSTOM_ENCODING_SCHEMA = vol.Schema({
+    vol.Required("custom_encoding"): str,
+})
+
 class SyslogConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    VERSION = 3
+    VERSION = 2
 
     async def async_step_user(self, user_input=None):
         errors = {}
         if user_input is not None:
+            if user_input.get("encoding") == "Other…":
+                self._temp_user_input = user_input  # stash for next step
+                return await self.async_step_custom_encoding()
             return self.async_create_entry(
-                title=user_input["instance_name"], data=user_input
+                title=user_input["instance_name"],
+                data=user_input
             )
         return self.async_show_form(
             step_id="user",
             data_schema=STEP_USER_DATA_SCHEMA,
+            errors=errors
+        )
+
+    async def async_step_custom_encoding(self, user_input=None):
+        errors = {}
+        if user_input is not None:
+            encoding = user_input["custom_encoding"]
+            data = self._temp_user_input
+            data["encoding"] = encoding
+            return self.async_create_entry(
+                title=data["instance_name"],
+                data=data
+            )
+
+        return self.async_show_form(
+            step_id="custom_encoding",
+            data_schema=CUSTOM_ENCODING_SCHEMA,
             errors=errors
         )
 
