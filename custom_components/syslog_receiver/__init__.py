4f31c01f0321f8f250f262a665b0b9239eac09f8
zollak <44733278+zollak@users.noreply.github.com>
2025-07-23 16:33:47 +0200

    add async_migrate_entry class for config entry update

--- custom_components/syslog_receiver/__init__.py
+++ custom_components/syslog_receiver/__init__.py
@@ -31,3 +31,25 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             await hass.config_entries.async_forward_entry_unload(entry, platform)
 
     return True
+
+async def async_migrate_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
+    """Migrate old config entries to the latest version."""
+    _LOGGER.debug("Migrating configuration from version %s.%s", entry.version, entry.minor_version)
+
+    if entry.version > 1:
+        # This means the user has downgraded from a future version
+        _LOGGER.error("You would downgrade newer version of config_entry than version 1. It is not allowed. Please, remove the %s configuration and add create it again.", entry.titlec)
+        return False
+
+    # Migrate to version 2 by adding encoding
+    if entry.version < 2:
+        #_LOGGER.warning("async_migrate_entry CALLED for version %s", entry.version)
+        _LOGGER.warning("Migrating config entry %s version %s", entry.title, entry.version)
+        data = dict(entry.data)
+        if "encoding" not in data:
+            data["encoding"] = "utf-8"  # default for backward compatibility
+        hass.config_entries.async_update_entry(entry, data=data, version=2)
+        _LOGGER.warning("Successfully migrated syslog_receiver config entry %s to version 2", entry.title)
+        return True
+
+    return False # No migration needed
